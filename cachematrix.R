bc2921281104c59c61cd7a9bc66c38d6a3196510
wehrlem <marcel@duckstance.com>
2015-03-22 17:38:55 +0100

    First commit for Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment 2: Lexical Scoping
 
-## Write a short comment describing this function
+# The makeCacheMatrix functions returns a list containing following functions
+# set: set the matrix
+# get: get the matrix
+# settheInverse: set inverse of the matrix
+# gettheInverse: get inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inv <- NULL
+    set <- function(y) {
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() x
+    settheInverse <- function(inverse) inv <<- inverse
+    gettheInverse <- function() inv
+    list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+# The function cacheSolve return an inverse matrix of an input matrix. 
+# It checks at first step if there is already a cached matrix, otherwise
+# it computes the inverse then sets the values in the cache with setinverse. 
+# The input matrix has to be invertible.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inv <- x$gettheInverse()
+    if(!is.null(inv)) {
+        message("get cached data.")
+        return(inv)
+    }
+    data <- x$get()
+    inv <- solve(data)
+    x$settheInverse(inv)
+    inv
 }
+
+# Example:
+# B <- matrix(c(2, 4, 3, 1, 5, 7,2,3,9), nrow=3, ncol=3) 
+# m = makeCacheMatrix(B)
+# cacheSolve(m)
